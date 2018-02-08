1ee8ea57b67089ea48c9e9835817ed006ca895ce
taylorsansom <taylor.sansom@twdb.texas.gov>
2018-02-08 15:19:26 -0600

    added log_sample method

---
+++ build/lib/sondeserial/__init__.py
@@ -0,0 +1,11 @@
+# Author: Taylor Sansom (taylor.sansom@twdb.texas.gov)
+# Created: 2/1/2018
+
+'''
+The :mod: 'sondeserial' module implements the sonde connection
+and manipulation via serial.
+'''
+
+from .sondeserial import YSI600
+
+__all__ = ['YSI600']
