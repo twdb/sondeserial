541531b3e20ab913dc58d6e73d0a567e491319fd
taylorsansom <taylor.sansom@twdb.texas.gov>
2018-02-09 10:45:57 -0600

    fixed serial connection checker

--- sondeserial/sondeserial.py
+++ sondeserial/sondeserial.py
@@ -71,7 +71,7 @@ class YSI600:
                     sleep(0.2)
                     ser.write(b'menu\r\n')
                     sleep(0.2)
-                    assert ser.in_waiting > 6
+                    assert ser.in_waiting >= 18
                     self.port = comport
                     ser.close()
                     break
